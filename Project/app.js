46feb78254a760666a158cb5d4a082c1a717bf0c
sanjuly <sandracordoba16@gmail.com>
2024-09-19 20:38:40 -0300

    change in the theme

--- Project/app.js
+++ Project/app.js
@@ -74,3 +74,18 @@ function removeChanges(taskContent) {
     const updateTask = tasks.filter((task) => task !== taskContent)
     localStorage.setItem("tasks", JSON.stringify(updateTask))    
 }
+//toggle theme
+const themeToggleButton = document.getElementById("toggle-theme-btn")
+const currentTheme = localStorage.getItem("theme")
+console.log(currentTheme)
+
+themeToggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme")
+
+    const theme = document.body.classList.contains("dark-theme") ?
+    "dark" : "light"
+    localStorage.setItem("theme", theme)
+})
+if (currentTheme  === "dark") {
+    document.body.classList.add("dark-theme")
+}
