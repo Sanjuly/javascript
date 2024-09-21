680c5f4b3c8b67e07b0e990e169090b098ba2d89
sanjuly <sandracordoba16@gmail.com>
2024-09-21 11:55:57 -0300

    Removed class MyArray definition, added console log statement for array, and uncommented array declaration.

--- Project/estrctdatos.js
+++ Project/estrctdatos.js
@@ -1,8 +1,9 @@
-//const array = ["sandra", "miguel", "eduard"]
-class MyArray {
-    constructor() {
-        this.length = 0;
-        this.data = {}
-    }
+const array = ["sandra", "miguel", "eduard"]
+console.log(array)
+//class MyArray {
+  //  constructor() {
+    //    this.length = 0;
+      //  this.data = {}
+   // }
     
-}
+//}
